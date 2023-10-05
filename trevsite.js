35c1b1bf428a146fa04e3cdcb5d9fb00a82156b5
Trevor Penner <tce.penner@gmail.com>
2023-10-05 11:36:13 -0600

    added more file paths
    trying to get everything out of the vendors folder

--- trevsite.js
+++ trevsite.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-//const greeting = require("./greet.js");
-//const mysql = require("mysql");
-//const bodyparser = require("body-parser");
+
+const bodyparser = require("body-parser");
 const path = require("path");
 
 // app.set("views", __dirname + "/views");
@@ -15,7 +14,7 @@ const path = require("path");
 // app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"), {extensions:["css", "jpg"]}));
 app.use(express.static(path.join(__dirname, "media"), {extensions:["jpg"]}));
-app.use(express.static(path.join(__dirname, "vendor"), {extensions:["css", "js", "ts", "map"]}));
+app.use(express.static(path.join(__dirname, "vendor"), {extensions:["css", "js", "ts", "map", "cjs", "esm"]}));
 app.use(express.static(path.join(__dirname, "js"), {extensions:["js"]}));
 app.use(express.urlencoded({extended: true}));
 
@@ -46,31 +45,5 @@ app.get("/details5", function(req,res){
 app.get("/details6", function(req,res){
     res.sendFile(path.join(__dirname,"views", "details6.html"));
 });
-
-// app.get("/", (req, res) => {
-//     res.render("index");
-// });
-// app.get("/details1", function(req,res){
-//     res.render("details1");
-// });
-// app.get("/details2", function(req,res){
-//     res.render("details2");
-// });
-
-// app.get("/details3", function(req,res){
-//     res.render("details3");
-// });
-
-// app.get("/details4", function(req,res){
-//     res.render("details4");
-// });
-
-// app.get("/details5", function(req,res){
-//     res.render("details5");
-// });
-
-// app.get("/details6", function(req,res){
-//     res.render("details6");
-// });
 
 
